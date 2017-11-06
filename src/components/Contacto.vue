bf4bd1610ed9eafebf18e30ab2b64302df417758
Thirty33 <joelsuarez.1101@gmail.com>
2017-11-06 16:23:23 -0500

    navegacion programatica con

--- src/components/Contacto.vue
+++ src/components/Contacto.vue
@@ -2,12 +2,23 @@
 	<div>
 
 		<h1>Contacto</h1>
+
+		<h2>manejo de stack historico con $router</h2>
+		<button @click="volverHome">Volver al home</button>
 	</div>
 
 </template>
 
 <script>
 	export default {
+		created(){
+			setTimeout(()=>this.$router.push('/'),3000)
+		},
+		methods: {
+			volverHome(){
+				this.$router.push('/', () => console.log('ruta cambiada')) //home
+			}
+		}
 		
 	}
 </script>
