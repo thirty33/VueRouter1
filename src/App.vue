6e02a3e3f6685b0069645bf19d6b53ff4942e58a
Thirty33 <joelsuarez.1101@gmail.com>
2017-11-06 16:11:23 -0500

    active-class en links de router

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,19 @@
     <h1>vistas con vue router</h1>
     <hr>
 
-    <router-link to="/">Home</router-link>
-    <router-link to="/contacto">Contacto</router-link>
-    <router-link to="/portada">Portada</router-link>
+    <router-link to="/" exact active-class="active" tag="li">
+      <a href="">Home</a>
+    </router-link>
 
+    <router-link to="/contacto" exact active-class="active" tag="li">
+      <a href="">Contacto</a>
+    </router-link>
+
+    <router-link to="/portada" exact active-class="active" tag="li">
+      <a href="">Portada</a>
+    </router-link>
+
+ 
 
     <router-view></router-view>
 
@@ -32,4 +41,12 @@ export default {
   color: #2c3e50;
   margin-top: 60px;*/
 }
+
+  /*.router-link-active {
+    color:red;
+  }*/
+
+  .active a {
+    color: red;
+  }
 </style>
