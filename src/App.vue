582decc9346215a294b6ff66ca1614b120c3c17d
Thirty33 <joelsuarez.1101@gmail.com>
2017-11-06 15:56:51 -0500

    Usando vue routes

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>vistas con vue router</h1>
+    <hr>
+
+    <router-link to="/">Home</router-link>
+    <router-link to="/contacto">Contacto</router-link>
+    <router-link to="/portada">Portada</router-link>
+
+
+    <router-view></router-view>
+
+
+  </div>
+</template>
+
+<script>
+  import Portada from './components/Portada.vue'
+  import Contacto from './components/Contacto.vue'
+export default {
+  components: {Portada, Contacto},
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;*/
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../components/home.vue'
+import Contacto from '../components/Contacto.vue'
+import Portada from '../components/Portada.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/contacto',
+      name: 'contacto',
+      component: Contacto
+    },
+    {
+      path: '/portada',
+      name: 'portada',
+      component: Portada
+    },
+   
+  ]
+})
